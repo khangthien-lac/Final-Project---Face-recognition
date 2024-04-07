a774cb24db789661cedabe47a2be0bf2c3531d4a
khangthien-lac <161085032+khangthien-lac@users.noreply.github.com>
2024-04-07 23:06:19 +0700

    Update project.py

--- project.py
+++ project.py
@@ -60,7 +60,7 @@ def game_function(count,camera):
         return score
 
 
-folder = r'\pic'
+folder = r'pic'
 os.chdir(folder)
 with st.sidebar:
     selected = option_menu("Main Menu", ['Main Screen',"Home", 'Settings'], 
