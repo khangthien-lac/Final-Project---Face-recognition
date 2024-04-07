7223891a8e898fb8bc12001e7d705cc34b66cab4
khangthien-lac <161085032+khangthien-lac@users.noreply.github.com>
2024-04-07 22:59:37 +0700

    Update project.py

--- project.py
+++ project.py
@@ -1,85 +1,110 @@
 import streamlit as st
 from deepface import DeepFace
+from PIL import Image
 import cv2
 import numpy as np
-import datetime
-import pandas as pd
+import os
+from numpy.linalg import norm
+from streamlit_option_menu import option_menu
 
-def face_count(face_pic):
-	if face_pic is not None:
-		bytes_data = face_pic.getvalue()
-		img_arr = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-		facecount = len(DeepFace.extract_faces(img_arr,enforce_dectection = True))
-		if facecount == 1: return 1
-		return 0
-def face_reg(face_pic):
-	if face_pic is not None:
-		bytes_data = face_pic.getvalue()
-		img_arr = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-		return DeepFace.extract_faces(img_arr,enforce_detection=False)[0].get('face')
-def face_verification(face,face_need_to_verify,face_pic):
-	facecount = face_count(face_pic)
-	if facecount > 1: return None,None,None,None
-	for x,y in face.items():	
-		Same_or_not = DeepFace.verify(y,face_need_to_verify,enforce_detection=False).get('verified')
-		distance = DeepFace.verify(y,face_need_to_verify,enforce_detection=False).get('distance')
-		date,time = time_log()
-		if Same_or_not == True: 
-			st.success('Bạn đã điểm danh thành công')
-			return Same_or_not,x,date,time,(1- distance)
-		else: 
-			st.warning('Xin hãy đăng kí')
-			return Same_or_not,None,date,time
-def verified(image):
-	count = face_count(image)
-	return count,face_reg(image)	
-def time_log():
-	current_datetime = datetime.datetime.now()
-	date_str = current_datetime.strftime("%Y-%m-%d")
-	time_str = current_datetime.strftime("%H:%M:%S")
-	return date_str,time_str
-		
-face = {}
-st.title('Trang web điểm danh')
-tab1, tab2, tab3 = st.tabs([ 'Registration','Verification','Time log'])
+def register(face_pic,username):
+    bytes_data = face_pic.getvalue()
+    img_arr = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    user_face = len(DeepFace.extract_faces(img_arr,enforce_detection=False))
+    if user_face > 1: return 0
+    else: 
+        cv2.imwrite(f'{username}.jpg',img_arr)
+        return 1
+def face_recog(face_need_to_verify):
+    return DeepFace.extract_faces(face_need_to_verify,enforce_detection=True,grayscale=True)[0].get('face')
+def face_verify(face_need_to_verify,folder):
+    user_dict = load_images_from_folder(folder)
+    cur_user = face_recog(face_need_to_verify)
+    for x,y in user_dict.items():
+        similarity = y@cur_user/(norm(y)*norm(cur_user))
+        if similarity > 0.6: return round(similarity,2),x
+    return 0,0
+def load_images_from_folder(folder):
+    image = {}
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            imgs = face_recog(img)
+            username = os.path.splitext(filename)
+            image[username[0]] = imgs
+    return image
+def happy(face_pic):
+    emotion = DeepFace.analyze(face_pic,actions='emotion',enforce_detection=False)[0].get('dominant_emotion')
+    if emotion == 'happy': 
+        score = DeepFace.analyze(face_pic,actions='emotion',enforce_detection=False)[0].get('emotion').get('happy')*100000
+        return round(score,1)
+    return 0
+def game(submit,camera):
+    count = 0
+    scores = []
+    if submit == True:
+        count = count + 1
+        score = game_function(count,camera)
+        scores.append(score)
+    if count == 3: return max(scores)
 
-with tab1:
-	username = st.text_input(label='Username',value=None)
-	image = st.camera_input('Đăng kí')
-	if image is not None:
-		if username == None or username =='':
-			st.error('Bạn chưa nhập username', icon="🚨")
-		elif username != None or username !='':
-			onlyOne, face_arr = verified(image)
-			if onlyOne == 0: st.warning('Trong ảnh chỉ được có 1 người')
-			else: 
-				face[username] = face_arr
-				st.success('Bạn đã đăng kí thành công')
-with tab2:
-	cur_image = []
-	if image is not None and face != {}:
-		cur_image = st.camera_input('Đăng nhập')
-		if cur_image is not None:
-			face_verify = face_reg(cur_image)
-			verify,cur_username,date,time,percentage = face_verification(face,face_verify,cur_image)
-			st.write(f'{cur_username} : {round(percentage*100,2)}%')
-			if verify == None: st.warning('Trong ảnh chỉ được có 1 người')
-with tab3:
-	timelog = {
-		'Username' : list,
-		'Date' : list,
-		'Time' : list
-	}
-	cur_username_list = []
-	cur_date = []
-	cur_time = []
-	if face != []:
-		if image is not None and face != {}:
-			if cur_image is not None:
-				cur_username_list.append(cur_username)
-				cur_date.append(date)
-				cur_time.append(time)
-				timelog['Username'] = cur_username_list
-				timelog['Date'] = cur_date
-				timelog['Time'] = cur_time
-				st.table(pd.DataFrame.from_dict([timelog]))
+def game_function(count,camera):
+    if camera is not None:  
+        bytes_data = camera.getvalue()
+        img_arr = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        cv2.imwrite(f'cached_{count}.jpg',img_arr)
+        score = happy(img_arr)
+        st.success(f'Diem so cua ban la {score}')
+        if count == 3: return score
+        st.write(f'Ban con {3-count} lan nua')
+        return score
+
+
+folder = r'\pic'
+os.chdir(folder)
+with st.sidebar:
+    selected = option_menu("Main Menu", ['Main Screen',"Home", 'Settings'], 
+        icons=['house','fire', 'gear'], menu_icon="cast", default_index=1)
+    selected
+if selected == 'Main Screen':
+    title_format = f'<p style="text-align: center; font-family: ' \
+               f'Arial; color: #FFBF00; font-size: 100px; ' \
+               f'font-weight: bold;">HAPPY APP</p>'
+
+    st.markdown(title_format, unsafe_allow_html=True)
+if selected == 'Home':
+    tab1, tab2 = st.tabs(['Registration','Verification'])
+    with tab1:
+        username = st.text_input(label='Username',value=None)
+        image = st.camera_input('Đăng kí')
+        if image is not None:
+            if username == None or username =='':
+                st.error('Bạn chưa nhập username', icon="🚨")
+            elif username != None or username !='':
+                registered = register(image,username)
+                if registered == 0: st.warning('Trong ảnh chỉ được có 1 người')
+                else: st.success('Bạn đã đăng kí thành công')
+    with tab2:
+        if image is not None:
+            cur_image = st.camera_input('Đăng nhập')
+            if cur_image is not None:
+                bytes_data = cur_image.getvalue()
+                img_arr = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+                similarity,username = face_verify(img_arr,folder)
+                if similarity == 0: st.warning('Xin hay dang ki')
+                else:
+                    st.success(f'{username}: {similarity}')
+if selected == 'Settings':
+    st.success('Xin hay cuoi')
+    camera = st.camera_input('Cuoi')
+    count = 0
+    scores = []
+    submit = st.button('Submit')
+    while 1:
+        if submit == True:
+            count = count + 1
+            score = game_function(count,camera)
+            scores.append(score)
+        if count == 3: 
+            st.write(f'{max(scores)}')
+            break
